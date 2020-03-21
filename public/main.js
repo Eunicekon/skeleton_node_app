6265c19784ef09e192b98c6c4b3dc20cb5070a77
Eunice K <kontor.eunice@gmail.com>
2020-03-21 22:23:40 +0000

    Table created and new pokemon background image added

--- public/main.js
+++ public/main.js
@@ -1,15 +1,4 @@
 const request = require('request');
 
-function makeTable(){
-    const table = document.getElementById('myTable');
-    const input = document.getElementById('searchPoke').nodeValue;
-
-    console.log(input);
-}
-
-for(
-)
-
-document.getElementById('clickMe').addEventListener("click", makeTable);
 
 
