dea02dbc1b7eecf17df6f5706cec5783e17cb2fc
Eunice K <kontor.eunice@gmail.com>
2020-05-07 07:43:38 +0100

    pokemon table complete

--- app.js
+++ app.js
@@ -11,14 +11,10 @@ app.get('/', (req, res) => {
   request("https://pokeapi.co/api/v2/pokemon/", function (error, response, body) {
     if (!error && response.statusCode == 200){
       const countData = JSON.parse(body)
-      res.render("index", {countData: countData});
-    }
+      res.render("pokemon");
+      }
+    });
   });
-});
-
-app.get('/pokemon', function(req, res){
-  res.render("pokemon")
-})
 
 //defining my route to call the html file
 app.set('view engine', 'ejs');                   
