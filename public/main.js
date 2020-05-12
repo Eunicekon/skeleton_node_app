d410be05496b608b1d7bfdd7d46d4d06f3324c7e
Eunice K <kontor.eunice@gmail.com>
2020-05-12 10:28:26 +0100

    Modal start

--- public/main.js
+++ public/main.js
@@ -35,3 +35,23 @@ const myPokemon = (bulbaPoke) => {
 };
 
 pokeData();
+
+// Modal
+
+const modal = document.getElementById("pokeModal");
+const button = document.getElementById("pokeBtn");
+
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+ modal.style.display = "block";
+};
+
+span.onclick = function() {
+ modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
