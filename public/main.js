e6f92d632e6038c65357d96e283907e8087a3ac4
Eunice K <kontor.eunice@gmail.com>
2020-05-18 16:05:51 +0100

    Navigation bar working

--- public/main.js
+++ public/main.js
@@ -42,6 +42,12 @@ const navPosition = () => {
     
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) =>{
+            if (link.style.animation) {
+                link.style.animation = ''
+            }else{link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+        }
+        });
     });
 }
 
